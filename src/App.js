b26e9e9ae2fce5f72aa91be28665846edf0e721a
rayvaldez <ray_valdez@hotmail.co.uk>
2022-06-06 21:38:01 +0100

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import Landing from './components/landing';
+import Landing from './components/Landing';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <Landing />
     </div>
   );
