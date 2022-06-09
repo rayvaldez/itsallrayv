b95d194e535c23c30859ee0c006a1e66ee10e5ee
rayvaldez <ray_valdez@hotmail.co.uk>
2022-06-09 22:07:25 +0100

    Remove box shadow and flex logo & menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,25 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+
 
 const Header = () => {
 
 
   return (
     <div>
-      <AppBar sx={{background: 'none'}}>
-        <Toolbar>
-          <h2>Header Logo</h2>
-          <IconButton>
+      <AppBar sx={{background: 'none', boxShadow: '0 0 0 0'}}>
+        <Toolbar sx={{width: '98%', margin: '0 auto'}}>
+          <Typography variant='h5' sx={{
+            display: { xs: 'none', md: 'flex' },
+            flexGrow: 1,
+          }}>
+          Header Logo
+          </Typography>
+          <IconButton sx={{
+            display: { xs: 'flex', md: 'none' },
+          }}>
             <MenuIcon />
           </IconButton>
         </Toolbar>
