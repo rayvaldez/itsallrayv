dcbf92e6d84724751c8a4f653db947996f6271b2
rayvaldez <ray_valdez@hotmail.co.uk>
2022-06-12 18:17:05 +0100

    Import Introduction component to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Landing from './components/Landing';
 import Header from './components/Header';
+import Introduction from "./components/Introduction";
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 function App() {
 
@@ -11,10 +14,13 @@ function App() {
   });
   return (
     <div className="App">
+    <CssBaseline>
       <ThemeProvider theme={theme}>
         <Header />
         <Landing />
+        <Introduction />
       </ThemeProvider>
+    </CssBaseline>
     </div>
   );
 }
