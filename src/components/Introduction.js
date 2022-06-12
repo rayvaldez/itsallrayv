bf325deb5304f53a556fba9fe1c31318cf9cdcdd
rayvaldez <ray_valdez@hotmail.co.uk>
2022-06-12 18:17:42 +0100

    Create Introduction component

---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+export default function Introduction() {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '100vh',
+        backgroundColor: '#FAF9F6',
+    }}>  
+   </Box>
+  )
+}
